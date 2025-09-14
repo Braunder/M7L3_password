ee350fe830c2556be4c9d24daa84011bacfbf211
Andrey <braunder.andrey@gmail.com>
2025-09-14 16:42:39 +0300

    Update test_new_password.py

--- tests/test_new_password.py
+++ tests/test_new_password.py
@@ -1,22 +1,14 @@
+import random
 import string
-from password.new_password import generate_password
 
-def test_password_characters():
-    """Тест, что при генерации используются только допустимые символы"""
-    valid_characters = string.ascii_letters + string.digits + string.punctuation
-    password = generate_password(100)  # Генерируем длинный пароль для более надежной проверки
-    for char in password:
-        assert char in valid_characters
+def generate_password(length=12):
+    """Генерация случайного пароля заданной длины."""
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''
+    for i in range(length):
+        password += random.choice(characters)
+    return password
 
-def test_password_length():
-    """Тест, что пароль имеет длину не менее 12 символов"""
-    for length in range(1, 21): 
-        assert len(generate_password(length)) == length
-
-
-"""Допиши еще один тест из предложенных. Или придумай свой.
-Если сможешь написать больше, то будет круто!
-
-Тест, что длина пароля соответствует заданной
-Тест, что два сгенерированных подряд пароля различаются
-"""
+# Пример использования
+password_length = 12  # Вы можете выбрать любую длину пароля
+print("Ваш новый пароль:", generate_password(password_length))
